d6394bfcdd9d866bbdb72fffc268daa0ba02afc8
John Edward Ospina <johneospina78@gmail.com>
2022-11-11 19:10:56 -0500

    footer creado con desvanecido

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'prueba',
+    component: FooterComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
